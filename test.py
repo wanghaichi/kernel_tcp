de43e05a981ea05c92a4a17827e4703420fee722
du-yiheng <1533152445@qq.com>
2024-03-21 18:45:18 +0800

    dyh-20240321

--- test.py
+++ test.py
@@ -8,25 +8,9 @@ from liebes.analysis import CIAnalysis
 from liebes.sql_helper import SQLHelper
 from liebes.tokenizer import *
 import numpy as np
+from liebes.llvm_process import LLVMHelper
 
 if __name__ == '__main__':
-    linux_path = '/home/wanghaichi/linux-1'
-    sql = SQLHelper()
-    start_time = datetime.now()
-
-    checkouts = sql.session.query(DBCheckout).order_by(DBCheckout.git_commit_datetime.desc()).limit(20).all()
-    cia = CIAnalysis()
-    for ch in checkouts:
-        cia.ci_objs.append(Checkout(ch))
-    cia.reorder()
-    cia.set_parallel_number(40)
-    cia.filter_job("COMBINE_SAME_CASE")
-    cia.filter_job("FILTER_FAIL_CASES_IN_LAST_VERSION")
-    cia.filter_job("COMBINE_SAME_CONFIG")
-    cia.filter_job("COMBINE_SAME_CASE")
-
-    cia.statistic_data()
-
-    for ci_obj in cia.ci_objs:
-        ci_obj.get_all_testcases()
+    llvm_helper = LLVMHelper(root_path=r'/home/duyiheng/projects/ltp_projects/ltp_20240129/ltp-20240129')
+    llvm_helper.generate_bitcode()
 
